1fba624d1f190594c77a6d63df18e66afa205010
YuanShiDong <yuan@webimpact.co.jp>
2017-10-23 12:07:04 +0800

    clock example

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const element = <h1>Hello, world</h1>;
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+}
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+setInterval(tick, 1000);
 registerServiceWorker();
