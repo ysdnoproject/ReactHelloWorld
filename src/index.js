2c262b4543ed4bd938401445e284e98413c146c8
YuanShiDong <yuan@webimpact.co.jp>
2017-10-23 14:25:17 +0800

    add clock js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Clock from "./Clock";
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
-}
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
 
-setInterval(tick, 1000);
+// setInterval(Clock, 1000);
 registerServiceWorker();
