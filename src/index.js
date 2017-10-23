14e5ecf6eae9125208c03cc7e7fca6e2b038ea77
YuanShiDong <yuan@webimpact.co.jp>
2017-10-23 11:53:10 +0800

    try Introducing JSX

--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
 registerServiceWorker();
