79ea6ab9e96ee7e76b5f80853eb07dafdbc7efca
YuanShiDong <yuan@webimpact.co.jp>
2017-11-17 12:16:51 +0900

    complete filterable_product_table

--- src/flterable_product_table/FilterableProductTable.js
+++ src/flterable_product_table/FilterableProductTable.js
@@ -3,11 +3,40 @@ import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
 class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {filterText: '', inStockOnly: false};
+
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+        this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+
+    handleFilterTextChange(filterText) {
+        this.setState({
+            filterText: filterText
+        });
+    }
+
+    handleInStockChange(inStockOnly) {
+        this.setState({
+            inStockOnly: inStockOnly
+        })
+    }
+
     render() {
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products} />
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    onFilterTextChange={this.handleFilterTextChange}
+                    onInStockChange={this.handleInStockChange}
+                />
+                <ProductTable
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
             </div>
         );
     }
--- src/flterable_product_table/ProductTable.js
+++ src/flterable_product_table/ProductTable.js
@@ -4,10 +4,19 @@ import ProductCategoryRow from "./ProductCategoryRow";
 
 class ProductTable extends React.Component {
     render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+
         const rows = [];
         let lastCategory = null;
 
         this.props.products.forEach((product) => {
+            if (product.name.indexOf(filterText) === -1) {
+                return;
+            }
+            if (inStockOnly && !product.stocked) {
+                return;
+            }
             if (product.category !== lastCategory) {
                 rows.push(
                     <ProductCategoryRow
@@ -18,7 +27,8 @@ class ProductTable extends React.Component {
             rows.push(
                 <ProductRow
                     product={product}
-                    key={product.name} />
+                    key={product.name}
+                />
             );
             lastCategory = product.category;
         });
